a25b8e72b5aa81685fd56ab3af9ee80f79dfb1ee
RaoSubhash7 <subhashkumar2005deoghar@gmail.com>
2026-06-15 17:17:56 +0530

    Project For Props Drilling in depth

--- 7.Project For Props Drilling/src/components/Card.jsx
+++ 7.Project For Props Drilling/src/components/Card.jsx
@@ -1,39 +1,31 @@
-import React from 'react'
 import { Bookmark } from 'lucide-react'
-function Card() {
-  return (
-    <>
-          <div className="parent">
-            <div className="card">
-    {/* ------------------------------------------ */}
-              <div className="top">
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ82LGte7aW4vSVQKUViGizHcKtt3YvIuzF3kB6hogrpg&s" alt="Amazon" />
-                <button>Save <Bookmark /> </button>
-              </div>
-    {/* ------------------------------------------------- */}
-              <div className="center">
-                <div>
-                  <h3>Amazon <span>5days ago</span></h3>
-                  <h2>Senior Software Engineer</h2>
-                  <div>
-                    <h4>Full Time</h4>
-                    <span>Remote</span>
-                  </div>
+const Card = (props) => {
+
+    return (
+        <div className="card">
+            <div>
+                <div className="top">
+                    <img src={props.brandLogo} alt="nachioooooo" />
+                    <button>Save <Bookmark size={10} /> </button>
                 </div>
-              </div>
-    {/* --------------------------------------------------------- */}
-              <div className="bottom">
+                <div className="center">
+                    <h3>{props.company} <span>{props.datePosted}</span></h3>
+                    <h2>{props.post}</h2>
+                    <div className='tag'>
+                        <h4>{props.tag1}</h4>
+                        <h4>{props.tag2}</h4>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom">
                 <div>
-                  <h3>120$/Hour</h3>
-                  <p>Ranchi Jharkhand</p>              
+                    <h3>{props.pay}</h3>
+                    <p>Mumbai, India</p>
                 </div>
                 <button>Apply Now</button>
-              </div>
-    
             </div>
-          </div>
-        </>
-  )
+        </div>
+    )
 }
 
 export default Card
---
+++ 7.Project For Props Drilling/src/components/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function User(props) {
+  return (
+    <div style={{color:'red'}}>
+        {props.name}
+    </div>
+  )
+}
+
+export default User
