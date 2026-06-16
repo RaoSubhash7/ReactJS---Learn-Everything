5ca17ddeea1a38617fb7fd15cdbb6bbd57a4c229
RaoSubhash7 <subhashkumar2005deoghar@gmail.com>
2026-06-16 21:47:16 +0530

    Tailwind CSS

---
+++ 9.Tailwins CSS/src/components/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='bg-gray-700 text-2xl text-white ml-20 h-fit mr-20 text-center mt-10'>Login</div>
+  )
+}
+
+export default Login
